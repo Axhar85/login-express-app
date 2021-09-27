029e66e729366b7a22683ea1ddac39deb27b19e7
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-27 17:36:18 +0200

    server is alive

--- login_system/server.js
+++ login_system/server.js
@@ -2,3 +2,16 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT||3000;
+
+// inestalize engine
+app.set('view engine', 'ejs');
+
+//home route
+app.get('/', (req, res) => {
+    res.render('base', {title:"Login System"});
+})
+
+//starting server
+app.listen(port, () =>{
+    console.log("Listening to the server on http://localhost:3000");
+})
