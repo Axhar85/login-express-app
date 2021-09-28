cb45016a6dcb16214f0879642507f5c3ec1fcd3c
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-28 19:18:23 +0200

    created and test login and create route

--- login_system/server.js
+++ login_system/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const path = require('path');
+const bodyparser = require('body-parser')
 const app = express();
+const session =  require('express-session');
+const { v4: uuidv4} = require("uuid");
+const router = require('./router')
+
 
 const port = process.env.PORT||3000;
+
+// for middleware
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
 
 // inestalize engine
 app.set('view engine', 'ejs');
@@ -11,6 +20,16 @@ app.set('view engine', 'ejs');
 app.use('/static',express.static((path.join(__dirname,'public'))))
 // pic
 app.use('/assets',express.static(path.join(__dirname,'public/assets')))
+
+//express-session
+app.use(session({
+    secret:uuidv4(),
+    resave: false,
+    saveUninitialized: true
+}));
+
+//router
+app.use('/route', router);
 
 //home route
 app.get('/', (req, res) => {
